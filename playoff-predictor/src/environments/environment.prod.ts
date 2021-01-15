31a2a88f8f0e9f5e7cd3c20955637eb3ff87deff
jagui1 <agui1@umbc.edu>
2021-01-15 05:35:38 -0500

    generated deployables

--- playoff-predictor/src/environments/environment.prod.ts
+++ playoff-predictor/src/environments/environment.prod.ts
@@ -1,11 +1,10 @@
-import { domain, clientId, audience, apiUri } from '../../auth_config.json';
+import { domain, clientId, apiUri } from '../../auth_config.json';
 
 export const environment = {
   production: true,
   auth: {
     domain,
     clientId,
-    audience,
     redirectUri: window.location.origin,
   },
   httpInterceptor: {
